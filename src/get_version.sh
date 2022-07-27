2ff2e18f785847ac37bb69c841111ad89c5d8328
qiaofeng1227 <76487013@qq.com>
2022-07-27 09:56:33 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo -e "elk_version: `docker exec -it elasticstack-elasticsearch bin/elasticsearch --version | awk -F ' ' '{print $2}'`" |sudo tee -a /data/logs/install_version.txt
+sudo echo "elk_version:" $(docker exec -it elasticstack-elasticsearch bin/elasticsearch --version | awk -F ' ' '{print $2}') |sudo tee -a /data/logs/install_version.txt
